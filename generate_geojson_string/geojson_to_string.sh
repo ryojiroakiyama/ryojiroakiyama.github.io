d751c9779035039181ce2d7bbd0fddf2721bdc7f
ryojiroakiyama <ryojiro25@gmail.com>
2023-05-07 11:49:56 +0900

    add new geojson

--- generate_geojson_string/geojson_to_string.sh
+++ generate_geojson_string/geojson_to_string.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# $1がなければエラー
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 [geojson file]"
+  exit 1
+fi
+
 # ファイルAを読み込んで改行と空白を削除し、ダブルクオーテーションをエスケープしてファイルBに出力する
-cat geojson.json | tr -d '\n' | tr -d '[:blank:]' | sed 's/"/\\"/g' | awk '{print "\"" $0 "\""}' > output.json
+cat $1 | tr -d '\n' | tr -d '[:blank:]' | sed 's/"/\\"/g' | awk '{print "\"" $0 "\""}' > output.json
